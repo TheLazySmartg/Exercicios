d4e90cb70375cb4fd906e48ff440c3fbc62ce5d1
SMARTG <51825210+TheLazySmartg@users.noreply.github.com>
2022-08-17 20:26:30 -0300

    Update Exercicios.py
    Coloquei mais exercícios resolvidos.

--- Exercicios.py
+++ Exercicios.py
@@ -35,3 +35,43 @@
 #n1 = float(input("Quantos quilos tem o produto: "))
 #n2 = float(input("Quanto é o quilo do produto: "))
 #print(f"Esse(a) {n} custa {n1 * n2}R$")
+
+#Exercicio 07
+
+#idade = int(input("Digite sua idade: "))
+#if idade >= 18:
+#    print("Você ja é maior de idade.")
+#else:
+#    print("Você ainda é menor de idade.")
+
+#Exercicio 08
+
+#n1 = float(input("Digite sua primeira nota: "))
+#n2 = float(input("Digite sua segunda nota: "))
+#media = (n1 + n2)/2
+#if media >= 70:
+#    print("APROVADO!!!")
+#else:
+#    print("REPROVADO!!!")
+
+#Exercicio 09
+
+#pdl = int(input("Quantos Pdl você tem: "))
+#if pdl < 1000:
+#    print("Ferro")
+#elif 1000 >= pdl or pdl < 2000:
+#    print("Bronze")
+#elif 2000 >= pdl or pdl < 3000:
+#    print("Prata")
+#elif 3000 >= pdl or pdl < 4000:
+#    print("Ouro")
+#elif 4000 >= pdl or pdl < 5000:
+#    print("Platina")
+#elif 5000 >= pdl or pdl < 6000:
+#    print("Diamante")
+#elif 6000 >= pdl or pdl < 7000:
+#    print("Mestre")
+#elif 7000 >= pdl or pdl < 8000:
+#    print("Grão-Mestre")
+#else:
+#    print("Desafiante")
